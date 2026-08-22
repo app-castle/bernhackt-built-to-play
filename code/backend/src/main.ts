dfb6974f419480c6daf26f3a7211c8d89f68e46b
Markus Chiarot <cmarkus@hotmail.ch>
2026-08-23 01:18:46 +0200

    remove vercel.json, add express server

--- code/backend/src/main.ts
+++ code/backend/src/main.ts
@@ -1,14 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { AppModule } from './app.module';
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors({
     origin: '*',
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
+
+export default server;
