3ea7e74332da244173004d48b35f7dc562c03200
Markus Chiarot <cmarkus@hotmail.ch>
2026-08-22 15:19:09 +0200

    enable all origin (cors)

--- code/backend/src/main.ts
+++ code/backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
